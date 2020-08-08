871dc6787bb59ed69dd81763ef6043e85e427379
kisqragi <kisaragi.sec@gmail.com>
2020-08-08 04:39:10 +0000

    'long long'を追加

--- parse.c
+++ parse.c
@@ -259,6 +259,8 @@ static Type *typespec(Token **rest, Token *tok) {
                 break;
             case LONG:
             case LONG + INT:
+            case LONG + LONG:
+            case LONG + LONG + INT:
                 ty = ty_long;
                 break;
             default:
--- tests/tests.c
+++ tests/tests.c
@@ -296,6 +296,9 @@ int main() {
     assert(8, ({ long int x; sizeof(x); }), "({ long int x; sizeof(x); })");
     assert(8, ({ int long x; sizeof(x); }), "({ int long x; sizeof(x); })");
 
+    assert(8, ({ long long x; sizeof(x); }), "({ long long x; sizeof(x); })");
+    assert(8, ({ long long int x; sizeof(x); }), "({ long long int x; sizeof(x); })");
+    assert(8, ({ int long long x; sizeof(x); }), "({ int long long x; sizeof(x); })");
 
     printf("OK\n");
     return 0;
