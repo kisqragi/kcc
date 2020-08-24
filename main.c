5b1dce528ccf5fa6ef9a902d90f2c63ad2f2df29
kisqragi <kisaragi.sec@gmail.com>
2020-08-24 15:09:08 +0000

    Add -o, --help options

--- main.c
+++ main.c
@@ -1,10 +1,53 @@
 #include "kcc.h"
 
-int main(int argc, char **argv) {
-    if (argc != 2)
-        error("%s: invalid number of arguments\n", argv[0]);
+static FILE *output_file;
+static char *input_path;
+static char *output_path = "-";
 
-    Token *tok = tokenize_file(argv[1]);
+static void usage(int status) {
+    fprintf(stderr, "kcc [ -o <path> ] <file>\n");
+    exit(status);
+}
+
+static void parse_args(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "--help"))
+            usage(0);
+
+        if (!strcmp(argv[i], "-o")) {
+            if (!argv[++i])
+                usage(1);
+            output_path = argv[i];
+            continue;
+        }
+
+        if (!strncmp(argv[i], "-o", 2)) {
+            output_path = argv[i] + 2;
+            continue;
+        }
+
+        if (argv[i][0] == '-' && argv[i][1] != '\0')
+            error("unknown argument: %s", argv[i]);
+
+        input_path = argv[i];
+    }
+
+    if (!input_path)
+        error("no input files");
+}
+
+int main(int argc, char **argv) {
+    parse_args(argc, argv);
+
+    if (!strcmp(output_path, "-")) {
+        output_file = stdout;
+    } else {
+        output_file = fopen(output_path, "w");
+        if (!output_file)
+            error("cannot open output file: %s: %s", output_path, strerror(errno));
+    }
+
+    Token *tok = tokenize_file(input_path);
     Program *prog = parse(tok);
 
     for (Function *fn = prog->fns; fn; fn = fn->next) {
@@ -18,7 +61,7 @@ int main(int argc, char **argv) {
         fn->stack_size = align_to(offset, 16);
     }
 
-    printf(".file 1 \"%s\"\n", argv[1]);
+    fprintf(output_file, ".file 1 \"%s\"\n", argv[1]);
 
     codegen(prog);
 
